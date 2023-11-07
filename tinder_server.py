4a303cefd37ab0699b55ec97e539fcf5d1c1ba7c
David Castanheira de Souza <david.castanheira@aluno.faculdadeimpacta.com.br>
2023-11-07 01:11:01 +0000

    Update file tinder_server.py

--- tinder_server.py
+++ tinder_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 import estrutura_interesses as i
 
 app = Flask(__name__)
@@ -23,6 +23,68 @@ def adiciona_pessoa():
 def pessoa_por_id(id_pessoa):
     pessoa = i.localiza_pessoa(id_pessoa)
     return pessoa
+
+# Estrutura de dados para armazenar informações
+pessoas = []
+interesses = {}
+matches = {}
+
+# Rota para obter a lista de todas as pessoas
+@app.route('/pessoas', methods=['GET'])
+def get_pessoas():
+    return jsonify(pessoas)
+
+# Rota para adicionar uma pessoa
+@app.route('/pessoas', methods=['POST'])
+def add_pessoa():
+    data = request.get_json()
+    pessoas.append(data)
+    return 'Pessoa adicionada com sucesso', 201
+
+# Rota para obter informações de uma pessoa específica
+@app.route('/pessoas/<int:pessoa_id>', methods=['GET'])
+def get_pessoa(pessoa_id):
+    for pessoa in pessoas:
+        if pessoa['id'] == pessoa_id:
+            return jsonify(pessoa)
+    abort(404)  # Pessoa não encontrada, retorna 404
+
+# reseta a lista de pessoas
+@app.route('/reseta', methods=['POST'])
+def reseta_pessoas():
+    pessoas.clear()
+    return 'Lista de pessoas resetada com sucesso', 200
+
+# Rota para sinalizar interesse em outra pessoa
+@app.route('/sinalizar_interesse/<int:pessoa_id1>/<int:pessoa_id2>', methods=['PUT'])
+def sinalizar_interesse(pessoa_id1, pessoa_id2):
+    if pessoa_id1 not in interesses:
+        interesses[pessoa_id1] = []
+    interesses[pessoa_id1].append(pessoa_id2)
+    return 'Interesse registrado com sucesso', 201
+
+# Rota para remover interesse em outra pessoa
+@app.route('/sinalizar_interesse/<int:pessoa_id1>/<int:pessoa_id2>', methods=['DELETE'])
+def remover_interesse(pessoa_id1, pessoa_id2):
+    if pessoa_id1 in interesses and pessoa_id2 in interesses[pessoa_id1]:
+        interesses[pessoa_id1].remove(pessoa_id2)
+        return 'Interesse removido com sucesso', 200
+    abort(404)  # Pessoa ou interesse não encontrados, retorna 404
+
+# Rota para obter lista de interesses de uma pessoa
+@app.route('/interesses/<int:pessoa_id>', methods=['GET'])
+def get_interesses(pessoa_id):
+    if pessoa_id in interesses:
+        return jsonify(interesses[pessoa_id])
+    abort(404)  # Pessoa não encontrada, retorna 404
+
+# Rota para obter lista de matches de uma pessoa
+@app.route('/matches/<int:pessoa_id>', methods=['GET'])
+def get_matches(pessoa_id):
+    if pessoa_id in interesses:
+        matches[pessoa_id] = [pessoa for pessoa in interesses[pessoa_id] if pessoa in interesses and pessoa_id in interesses[pessoa]]
+        return jsonify(matches[pessoa_id])
+    return jsonify([])
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5003, debug=True)
